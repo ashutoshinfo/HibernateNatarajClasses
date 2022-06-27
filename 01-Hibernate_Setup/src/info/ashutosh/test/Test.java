1ca56a75af697dd6210547fae9fbcc8e0fe256c5
ashutoshjp <ashutoshpatel5@yahoo.in>
2022-06-27 11:38:47 +0530

    first commit

---
+++ 01-Hibernate_Setup/src/info/ashutosh/test/Test.java
@@ -0,0 +1,28 @@
+package info.ashutosh.test;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import info.ashutosh.entity.Product;
+
+public class Test {
+	public static void main(String[] args) {
+		Configuration configuration = new Configuration();
+		configuration.configure("/info/ashutosh/configuration/cfg/hibernate.cfg.xml");
+
+		SessionFactory sessionFactory = configuration.buildSessionFactory();
+
+		Session openSession = sessionFactory.openSession();
+
+		Product product = new Product();
+		product.setName("Ashutosh3");
+		product.setPid(3L);
+
+		Transaction beginTransaction = openSession.beginTransaction();
+
+		openSession.saveOrUpdate(product);
+		beginTransaction.commit();
+	}
+}
