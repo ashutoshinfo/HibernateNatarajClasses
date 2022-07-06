120da6f0ef6fdae5ea6fa2989b00c1b3789d6a5b
ashutoshjp <ashutoshpatel5@yahoo.in>
2022-07-06 12:15:43 +0530

    Test class

--- 15-Hibernate_Association_Mapping/src/main/java/info/ashutosh/test/Test.java
+++ 15-Hibernate_Association_Mapping/src/main/java/info/ashutosh/test/Test.java
@@ -1,9 +1,6 @@
 package info.ashutosh.test;
 
-import java.util.List;
-
 import org.hibernate.Session;
-import org.hibernate.query.SelectionQuery;
 
 import info.ashutosh.utility.HibernateUtil;
 
@@ -12,17 +9,6 @@ public class Test {
 
 		// HibernateUtil.getSessionFactory();
 		Session openSession = HibernateUtil.getSession();
-		// Product product = openSession.get(Product.class, 1L);
-		// SelectionQuery<?> createSelectionQuery = openSession.createSelectionQuery("from Actor");
-		// SelectionQuery<?> createSelectionQuery = openSession.createSelectionQuery("from Actor where price=500000");
-		SelectionQuery<?> createSelectionQuery = openSession.createSelectionQuery("from Actor where price= ?1");
-		createSelectionQuery.setParameter(1, "500000");
-
-		List<?> resultList = createSelectionQuery.getResultList();
-
-		for (Object object : resultList) {
-			System.out.println(object);
-		}
 
 		HibernateUtil.closeSession(openSession);
 		HibernateUtil.closeSessionFectory();
